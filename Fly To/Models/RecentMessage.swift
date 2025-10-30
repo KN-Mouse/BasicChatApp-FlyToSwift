2bc76de0730707441e04222429b382be06c0f652
MouseDev <khangnguyen.personal@gmail.com>
2025-10-30 17:40:12 +0700

    update

---
+++ Fly To/Models/RecentMessage.swift
@@ -0,0 +1,8 @@
+//
+//  RecentMessage.swift
+//  Fly To
+//
+//  Created by Nguyen Khang on 28/10/25.
+//
+
+import Foundation
---
+++ Fly To/ViewModels/RecentMessageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RecentMessageViewModel.swift
+//  Fly To
+//
+//  Created by Nguyen Khang on 28/10/25.
+//
+
+import Foundation
